5df5eaad322d724cd02f1334ab1d8982a2a46843
Lars Holmberg <K3LARRA@HOTMAIL.COM>
2016-10-04 11:53:40 +0200

    Lesson 1

---
+++ Week1/MyProject/try/script.js
@@ -0,0 +1,5 @@
+console.log("Hello world!");
+var myLittleListWithCoolStuff = document.getElementById('mylist');
+for(var i=0; i<100;i=i+1){
+  myLittleListWithCoolStuff.insertAdjacentHTML("beforeend","<p>Hello</p>");
+}
