2e81c33e1c3c6e1309a2ae3542d5c28e2f16103e
Lars Holmberg <K3LARRA@HOTMAIL.COM>
2016-10-04 12:36:17 +0200

    With numbers

--- Week1/MyProject/script.js
+++ Week1/MyProject/script.js
@@ -1,5 +1,5 @@
 console.log("Hello world!");
 var myLittleListWithCoolStuff = document.getElementById('mylist');
 for(var i=0; i<100;i=i+1){
-  myLittleListWithCoolStuff.insertAdjacentHTML("beforeend","<div>1</div>");
+  myLittleListWithCoolStuff.insertAdjacentHTML("beforeend","<div>"+i+"</div>");
 }
