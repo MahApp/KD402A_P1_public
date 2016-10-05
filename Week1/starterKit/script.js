a0aa06d5c05f16cec68eb2d5834794a308b6d957
Lars Holmberg <K3LARRA@HOTMAIL.COM>
2016-10-05 10:04:27 +0200

    Starter kit lesson 2

--- Week1/starterKit/script.js
+++ Week1/starterKit/script.js
@@ -6,13 +6,16 @@ if (document.readyState != 'loading'){
   document.addEventListener('DOMContentLoaded', onDocumentReady);
 }
 
-// Page is loaded! Now event can be wired-up
+// Page is loaded! Now events can be wired-up
 function onDocumentReady() {
+  //Start of function onDocumentReady above
   console.log('Document ready.');
-
+  //Here add all things you want to happen when the page is loaded like listening for keypress
+  //Get a handle to the button
   var el = document.getElementById('button1');
   // Listen to any click that happens on button1
   el.addEventListener('click', onButtonClicked);
+  //Bellow is the ending Måsvinge for the onDocumentReady function
 }
 
 // Called according to 'click' event
@@ -21,6 +24,6 @@ function onButtonClicked(e) {
   // Get a reference to the element we want to manipulate
   // Here, we're using the id 'text1' to find the right place.
   var text = document.getElementById('text1');
-  console.dir(text);
+  console.log(text);
   text.innerHTML = "New text";
 }
