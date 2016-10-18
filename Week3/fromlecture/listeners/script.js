e30944b50046ec434e3325aa39f5ac0acc141cb2
Lars Holmberg <K3LARRA@HOTMAIL.COM>
2016-10-18 12:43:29 +0200

    sdsd

--- Week3/fromlecture/listeners/script.js
+++ Week3/fromlecture/listeners/script.js
@@ -10,4 +10,37 @@ if (document.readyState != 'loading') {
 // Page is loaded! Now event can be wired-up
 function onDocumentReady() {
     console.log('Document ready.');
+    var sectionElement = document.getElementById("section");
+    //sectionElement.classList.add("hidden");
+    //sectionElement.classList.remove("hidden");
+    //sectionElement.classList.toggle("hidden");
+    //OpenClose
+    var expandElement = document.getElementById("expand");
+    expandElement.addEventListener("click",openClose);
+    //Input
+    var nameInputElement = document.getElementById("nameInput");
+    nameInputElement.addEventListener("keypress",keyPressedFunction);
+
+}
+
+function openClose(){
+  console.log("Click");
+  var sectionElement = document.getElementById("section");
+  if(sectionElement.classList.contains("hidden")){
+    sectionElement.classList.remove("hidden");
+  }else{
+    sectionElement.classList.add("hidden");
+  }
+
+  //sectionElement.classList.toggle("hidden");
+}
+
+function keyPressedFunction(e){
+  console.log("keypressed: "+e.key);
+  console.log("keypressed: "+e.code);
+  if(e.code==="Enter"){
+    console.log("Enter pressed");
+    //Hämta rätt element och byt css-class till röd på den.
+
+  }
 }
